8be8dc0bbf7b26b535b2a42ad035ea308a62e585
Mikhail Kalenik <Mikhail_Kalenik@epam.com>
2018-08-05 17:56:03 +0300

    Add comparer

---
+++ ReflectionComparer/Extension.cs
@@ -0,0 +1,11 @@
+﻿namespace ReflectionComparer
+{
+    public static class Extension
+    {
+        public static bool DeepCompare<T>(this T x, T y)
+        {
+            var comparer = new Comparer<T>();
+            return comparer.Equals(x, y);
+        }
+    }
+}
